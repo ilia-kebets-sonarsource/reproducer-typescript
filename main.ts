843a6242a9b41f671d8290d4fcc68fc2fab40bfc
Ilia Kebets <ilia.kebets@sonarsource.com>
2023-05-22 14:15:28 +0200

    try with other example

--- main.ts
+++ main.ts
@@ -2,3 +2,12 @@ function foo(bar: boolean | undefined, baz: boolean) {
   const qux = bar || baz;
   return qux;
 }
+
+type Bar = {
+  bar: boolean
+};
+
+function foo2(bar: Bar | undefined, baz: boolean) {
+  const qux = bar?.bar || baz;
+  return qux;
+}
