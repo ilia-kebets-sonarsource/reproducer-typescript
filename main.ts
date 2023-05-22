d9b4816c25f4ba30e075b9b5981dc06b75ddae04
Ilia Kebets <ilia.kebets@sonarsource.com>
2023-05-22 15:21:57 +0200

    add code from example

--- main.ts
+++ main.ts
@@ -1,9 +1,36 @@
-export interface Query {
-  sort: string | undefined
+export interface Perspective {
+  sort?: string;
+  view?: string;
+}
+export interface Query extends Perspective {
+  coverage?: number;
+  duplications?: number;
+  //gate?: Level;
+  languages?: string[];
+  maintainability?: number;
+  new_coverage?: number;
+  new_duplications?: number;
+  new_lines?: number;
+  new_maintainability?: number;
+  new_reliability?: number;
+  new_security?: number;
+  new_security_review_rating?: number;
+  reliability?: number;
+  search?: string;
+  security?: number;
+  security_review_rating?: number;
+  size?: number;
+  tags?: string[];
 }
 
-function hasVisualizationParams(view: string | undefined, query?: Query) {
-  return Boolean(query?.['sort'] || view);
+export function hasVisualizationParams(query?: Query) {
+  return Boolean(query?.['sort'] || getView(query?.['view']));
 }
 
-hasVisualizationParams('foo', { sort: 'bar' });
+hasVisualizationParams({ sort: 'bar' });
+
+export function getView(value: any): string | undefined {
+  return typeof value !== 'string' || value === 'overall' || value === 'visualizations'
+    ? undefined
+    : value;
+}
